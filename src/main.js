0147d81cc4c76a2f6eae2905e964bb481ca7f447
SchneeHertz <snowhzhang@gmail.com>
2022-10-28 16:53:17 +0800

    update lodash as global variable

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
+import _ from 'lodash'
 
 import { createI18n } from 'vue-i18n'
 import zhCn from './locales/zh-CN.json'
+
+window._ = _
 
 const app = createApp(App)
 app.use(createI18n({
